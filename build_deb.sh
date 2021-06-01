5240bdda39321411a7ac8f51309b5bd03d6c2f19
rigaya <rigaya34589@live.jp>
2021-06-01 15:05:21 +0900

    debファイルを作成するスクリプトを追加。

---
+++ build_deb.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+PACKAGE_NAME=ramspeed
+PACKAGE_OS=
+PACKAGE_MAINTAINER=rigaya
+PACKAGE_DEPENDS=
+PACKAGE_DESCRIPTION=
+PACKAGE_ROOT=.debpkg
+PACKAGE_VERSION=`git describe --tags | cut -f 1 --delim="-"`
+PACKAGE_ARCH=`uname -m`
+PACKAGE_ARCH=`echo ${PACKAGE_ARCH} | sed -e 's/x86_64/amd64/g'`
+
+mkdir -p ${PACKAGE_ROOT}/DEBIAN
+build_debpkg/replace.py \
+    -i build_debpkg/template/DEBIAN/control \
+    -o ${PACKAGE_ROOT}/DEBIAN/control \
+    --pkg-name ${PACKAGE_NAME} \
+    --pkg-version ${PACKAGE_VERSION} \
+    --pkg-arch ${PACKAGE_ARCH} \
+    --pkg-maintainer ${PACKAGE_MAINTAINER} \
+    --pkg-depends ${PACKAGE_DEPENDS} \
+    --pkg-desc ${PACKAGE_DESCRIPTION}
+
+mkdir -p ${PACKAGE_ROOT}/usr/bin
+cp ramspeed ${PACKAGE_ROOT}/usr/bin
+chmod +x ${PACKAGE_ROOT}/usr/bin/ramspeed
+
+DEB_FILE="${PACKAGE_NAME}_${PACKAGE_VERSION}${PACKAGE_OS}_${PACKAGE_ARCH}.deb"
+dpkg-deb -b "${PACKAGE_ROOT}" "${DEB_FILE}"
