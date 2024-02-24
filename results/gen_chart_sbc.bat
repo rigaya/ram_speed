9dab8fdff243a93feaed47b4bc24507709b34cc6
rigaya <rigaya34589@live.jp>
2024-02-24 11:28:51 +0900

    いくつかデータを追加。

---
+++ results/gen_chart_sbc.bat
@@ -0,0 +1,9 @@
+gen_chart.py ^
+  "Rock 5B A76,show,#66CC33,result_Cortex-A55x4_+_Cortex-A76x4_pcore.csv" ^
+  "Rock 5B A55,show,#B1E699,result_Cortex-A55x4_+_Cortex-A76x4_ecore.csv" ^
+  "Jetson Nano,show,#556B17,result_Jetson_nano_2GB_Cortex-A57_aarch64_physical.csv" ^
+  "R3 3200G,show,#CC3300,result_AMD_Ryzen_3_3200G_with_Radeon_Vega_Graphics_physical.csv" ^
+  "i5 7500,show,#00CCFF,result_Intel_Core_i5-7500_3.40GHz_physical.csv" ^
+  "i5 1035G7,show,#6699FF,result_Intel_Core_i5-1035G7_1.20GHz.csv" ^
+  > gen_chart_sbc.txt
+pause
